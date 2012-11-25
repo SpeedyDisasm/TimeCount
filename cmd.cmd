c9031ba0814e114d1ab3ddce645390a1c9fd7879
SteppenWolf <futureboyinbox@gmail.com>
2012-11-26 00:18:58 +0400

    Signed-off-by: SteppenWolf <futureboyinbox@gmail.com>

---
+++ cmd.cmd
@@ -0,0 +1,1 @@
+cmd.exe cd %CD%
---
+++ fsmA.asm
@@ -0,0 +1,11 @@
+	.686
+	.MMX
+	.XMM
+	.model	flat,stdcall
+	option	casemap:none
+
+	.code
+main:
+	mov eax,5  ; переместить в регистр ax значение 3030 в шестнадцатиричной системе счисления
+end main
+end
