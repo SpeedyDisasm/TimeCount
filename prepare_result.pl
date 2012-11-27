4ea608925875e1789eed3197f0fbe0c3e75cf406
dimmddr <dmitrius.one@gmail.com>
2012-11-27 23:11:37 +0400

    Работаю над автоматизацией подготовки результатов
    Подготовка результатов к построению по ним гистограмм

--- prepare_result.pl
+++ prepare_result.pl
@@ -3,26 +3,29 @@ use warnings;
 use diagnostics;
 
 $\ = $/;
-my $dirName = pop @ARGV;
-opendir  my $dir, $dirName or die "cannot open dir $dirName : $!";
-my @files = grep {/^(\d+).*/} readdir($dir);
-#say "@files";
-map {
-	open my $source, "<", "$dirName\\$_" or die "cannot open file $_ : $!";
-	open my $result, ">", "h_$_" or die "cannot open file h_.$_ : $!";
-	#open my $result, ">", "..\\histogramm\\$dirName\\$_" or die "cannot open dir for result: $!";
-	#mkdir "..\\histogramm\\$dirName";
-	#open my $result, ">", "..\\histogramm\\$dirName\\$_" or die "cannot open dir for result: $!";
-	
-	my @times = <$source>;
-	my %times;
+chdir "Timings";
+@directories = grep { -d $_ } glob "*";
+foreach my $dirName (@directories) {
+	opendir  my $dir, $dirName or die "cannot open dir $dirName : $!";
+	my @files = grep {/^(\d+).*/} readdir($dir);
+	#say "@files";
 	map {
-		chomp;
-		$times{$_}++;
-	}@times;
-	map {
-		print $result "$_ $times{$_}";# unless 0 == $times{$_};
-	} keys %times;
-	close $source;
-	close $result;
-} @files;
+		open my $source, "<", "$dirName\\$_" or die "cannot open file $_ : $!";
+		open my $result, ">", "h_$_" or die "cannot open file h_$_ : $!";
+		#open my $result, ">", "..\\histogramm\\$dirName\\$_" or die "cannot open dir for result: $!";
+		#mkdir "..\\histogramm\\$dirName";
+		#open my $result, ">", "..\\histogramm\\$dirName\\$_" or die "cannot open dir for result: $!";
+		
+		my @times = <$source>;
+		my %times;
+		map {
+			chomp;
+			$times{$_}++;
+		}@times;
+		map {
+			print $result "$_ $times{$_}";# unless 0 == $times{$_};
+		} keys %times;
+		close $source;
+		close $result;
+	} @files;
+}
